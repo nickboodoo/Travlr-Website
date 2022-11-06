ae113a9ba9262e0ddc5cb632bb06ca1fd5e9d69d
nickboodoo <boodoo@temple.edu>
2022-11-06 12:44:42 -0500

    Express website, static HTML only

---
+++ app_server/routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+const router = express.Router();
+const ctrlMain = require('../controllers/main');
+
+/* GET home page. */
+router.get('/', ctrlMain.index);
+
+module.exports = router;
